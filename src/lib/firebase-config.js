6e6737d7dedfab2b4291860579366eacf264c003
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2022-07-05 22:46:39 +0530

    Hello! Mom

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -1,11 +1,11 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAPgBFaHMrpAmdD85P3xKQtWfZ8bEnXHKQ",
-  authDomain: "espresso-f94df.firebaseapp.com",
-  projectId: "espresso-f94df",
-  storageBucket: "espresso-f94df.appspot.com",
-  messagingSenderId: "426698370233",
-  appId: "1:426698370233:web:daac3b9b7cda35a2937f68",
-  measurementId: "G-1R0V8S4J51"
+  apiKey: "AIzaSyBnCUmbQ0bOgvsTdyH8KK8s526PMWSxn6U",
+  authDomain: "espresso-2fa24.firebaseapp.com",
+  projectId: "espresso-2fa24",
+  storageBucket: "espresso-2fa24.appspot.com",
+  messagingSenderId: "170925917753",
+  appId: "1:170925917753:web:a99b57ec2eabdc959aafe8",
+  measurementId: "G-HZZ0BKVRBR"
 }
 
 export default FIREBASE_CONFIG
